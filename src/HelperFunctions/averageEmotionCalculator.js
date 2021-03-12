981578e0f69d46a4c5b71dd15f6f8d4d51bba7d5
dyson1602 <cclark000@citymail.cuny.edu>
2021-03-12 12:19:42 -0500

    new: emotionsOverTime fn returns array of reports with emotions averaged

---
+++ src/HelperFunctions/averageEmotionCalculator.js
@@ -0,0 +1,27 @@
+//returns array of reports with emotions averaged for each report
+function emotionsOverTime (reports){
+  let averagedReports = []
+  for(let report of reports){
+    averagedReports.push(averageReportCalculator(report))
+  }
+  return averagedReports
+}
+
+//generates a new object with emotion averages instead of emotion arrays
+function averageReportCalculator(report) {
+  let averageEmotionReport = {}
+  for (let key in report) {
+    if (key === "anger" || key === "disgust" || key === "fear" || key === "joy"
+      || key === "sadness" || key === "surprise") {
+      averageEmotionReport[key] = computeAverage(report[key])
+    } else averageEmotionReport[key] = report[key]
+  }
+  return averageEmotionReport
+}
+
+//computes average of each emotion array passed in as an argument
+function computeAverage(emotionArray) {
+  return (emotionArray.reduce((sum, val) => sum + val) / emotionArray.length)
+}
+
+export default emotionsOverTime
