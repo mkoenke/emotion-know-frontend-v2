7b3c2cb8c1bb12d0f5a496338cecd1d90198f76e
dyson1602 <cclark000@citymail.cuny.edu>
2021-03-22 23:42:30 -0400

    added: d3OverTimeLineGraph trying to make it function properly

--- src/HelperFunctions/emotionsOverTimeCalculator.js
+++ src/HelperFunctions/emotionsOverTimeCalculator.js
@@ -1,18 +1,23 @@
 //Calculates the return for the emotion over time graph
 function emotionsOverTimeCalculator(emotionReports) {
-  console.log(emotionReports)
   const countedEmotionReports = emotionReports.map(report =>
     singleReportFrequencyCounter(report)
   )
   const emotionPercentOfEntryArray = countedEmotionReports.map(report => {
     return percentOfCounts(report)
   })
+  //this solution needs refactoring, but adds date back to array
+  for(let i = 0; i < emotionReports.length; i++){
+    emotionPercentOfEntryArray[i].created_at = emotionReports[i].created_at
+  }
   return emotionPercentOfEntryArray
 }
 
 //Calculates the amount of times an emotion is the peak value
 function singleReportFrequencyCounter(report) {
-  //There may be a better way to destructure this, but it's already 11:09pm
+
+  //There may be a better way to destructure this for the newReportObj, 
+  //but it's already 11:09pm
   const newReportObj = {
     anger: report.anger,
     disgust: report.disgust,
@@ -29,7 +34,7 @@ function singleReportFrequencyCounter(report) {
     sadness: 0,
     surprise: 0
   }
-
+  //anger is arbitrary, could use any emotion to calc length
   for (let i = 0; i < newReportObj.anger.length; i++) {
     const sorted = Object.entries(newReportObj).sort((a, b) => {
       return b[1][i] - a[1][i]
@@ -57,7 +62,6 @@ function percentOfCounts(countedReport) {
     const percent = count/totalCount
     emotionalPercentCounter[`${emotion}`] = percent
   }
-  console.log(emotionalPercentCounter)
   return emotionalPercentCounter
 }
 
