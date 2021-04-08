666dbe572c76a96de7a3ab92f558a9213f5ca26f
dyson1602 <cclark000@citymail.cuny.edu>
2021-04-08 18:30:19 -0400

    removed: vestigial code that was replaced with a refactor

--- src/Containers/ReportGalleryPage.js
+++ src/Containers/ReportGalleryPage.js
@@ -143,99 +143,95 @@ class ReportGalleryPage extends React.Component {
             </div>
           </>
         ) : (
-            <>
-              <div className="background">
+            <div className="background">
               <Container>
                 <Header className="pageHeader" size="huge" textAlign="center">
                   Your Child {this.props.parent.child.username}'s Reports
                 </Header>
               </Container>
-                {!this.props.parentsReports.length ? (
-                  <Popup
-                    open
-                    size="huge"
-                    trigger={
-                      <Container textAlign="center">
-                        <Header as="h2" className="content tableHeaderMargin">
-                          Individual Journal Emotional Reports
-                        </Header>
+              <Container textAlign="center">
+                <Header as="h2" className="content tableHeaderMargin">
+                  Individual Journal Emotional Reports
+                </Header>
+                {ReportGalleryReportsTable(this.state.items, this.handleReportClick, this.onChangePage)}
+                <br />
+                {this.state.beenClicked ? ReportGallerySingleGraph(this.state) : null}
+              </Container>
+              <Container textAlign="center">
+                <Header as="h2" className="content tableHeaderMargin">
+                  Emotional Reports over Time
+                </Header>
+                <br />
+                <div className="lineGraph pattern">
+                  <D3OverTimeLineGraph data={this.state.items} />
+                </div>
+              </Container>
+              <div className="footer"></div>
+            </div>
 
-                        <br />
-                        {this.state.beenClicked
-                          ? this.renderParentReportGraph()
-                          : null}
-                      </Container>
-                    }
-                    content="When your child starts using EmotionKnow and creates a journal entry, their individual journal entry emotional charts will appear here!"
-                  />
-                ) : (
+            {!this.props.parentsReports.length ? (
+              <Popup
+                open
+                size="huge"
+                trigger={
                   <Container textAlign="center">
                     <Header as="h2" className="content tableHeaderMargin">
                       Individual Journal Emotional Reports
-                    </Header>
-                    {ReportGalleryReportsTable(this.state.items, this.handleReportClick, this.onChangePage)}
-                    {/* <Table celled className="table content">
-                      <Table.Header>
-                        <Table.Row>
-                          <Table.HeaderCell>Title</Table.HeaderCell>
-                          <Table.HeaderCell>Date</Table.HeaderCell>
-                        </Table.Row>
-                      </Table.Header>
-                      <Table.Body>{this.listOfParentsReports()}</Table.Body>
+                        </Header>
 
-                      <Table.Footer>
-                        <Table.Row>
-                          <Table.HeaderCell colSpan="3">
-                            <Menu floated="right">
-                              <JwPagination
-                                items={this.state.items}
-                                onChangePage={this.onChangePage}
-                                labels={customLabels}
-                                className="pagination"
-                              />
-                            </Menu>
-                          </Table.HeaderCell>
-                        </Table.Row>
-                      </Table.Footer>
-                    </Table> */}
                     <br />
                     {this.state.beenClicked
                       ? this.renderParentReportGraph()
                       : null}
                   </Container>
-                )}
-                {!this.props.parentsReports.length ? (
-                  <Popup
-                    open
-                    size="huge"
-                    trigger={
-                      <Container textAlign="center">
-                        <Header as="h2" className="content tableHeaderMargin">
-                          Emotional Reports over Time
-                        </Header>
-                        <D3OverTimeLineGraph data={this.state.items} />
-                      </Container>
-                    }
-                    content="Your child's emotions over time will appear here!"
-                  />
-                ) : (
+                }
+                content="When your child starts using EmotionKnow and creates a journal entry, their individual journal entry emotional charts will appear here!"
+              />
+            ) : (
+              <Container textAlign="center">
+                <Header as="h2" className="content tableHeaderMargin">
+                  Individual Journal Emotional Reports
+                    </Header>
+                {ReportGalleryReportsTable(this.state.items, this.handleReportClick, this.onChangePage)}
+                
+                <br />
+                {this.state.beenClicked
+                  ? this.renderParentReportGraph()
+                  : null}
+              </Container>
+            )}
+            {!this.props.parentsReports.length ? (
+              <Popup
+                open
+                size="huge"
+                trigger={
                   <Container textAlign="center">
                     <Header as="h2" className="content tableHeaderMargin">
                       Emotional Reports over Time
-                    </Header>
-                    <div className="lineGraph pattern">
-                      {this.state.clickedReport
-                        ? <D3LineGraph data={this.state.clickedReport} />
-                        : null
-                      }
-
-                    </div>
+                        </Header>
+                    <D3OverTimeLineGraph data={this.state.items} />
                   </Container>
-                )}
-                <div className="footer"></div>
-              </div>
-            </>
-          )}
+                }
+                content="Your child's emotions over time will appear here!"
+              />
+            ) : (
+              <Container textAlign="center">
+                <Header as="h2" className="content tableHeaderMargin">
+                  Emotional Reports over Time
+                    </Header>
+                <div className="lineGraph pattern">
+                  {this.state.clickedReport
+                    ? <D3LineGraph data={this.state.clickedReport} />
+                    : null
+                  }
+
+                </div>
+              </Container>
+            )}
+            <div className="footer"></div>
+          </div>
+
+        )}
       </>
     )
     // return (
