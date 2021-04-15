b456ad25c7fdaf2896b1ad92b7a47fe2c701d2ef
dyson1602 <cclark000@citymail.cuny.edu>
2021-04-15 16:22:07 -0400

    stack generator built with chart data object passing emotions keys in

--- src/Components/StackedBarChart.js
+++ src/Components/StackedBarChart.js
@@ -1,26 +1,34 @@
-import { select, scaleBand, axisBottom, svg } from 'd3'
+import { select, scaleBand, axisBottom, stack } from 'd3'
 import React, { useEffect, useRef } from 'react'
 import useResizeObserver from './useResizeObserver'
 
-function StackedBarChart({ data, keys, colors }) {
+function StackedBarChart({ data, colors }) {
   const svgRef = useRef()
   const wrapperRef = useRef()
   const dimensions = useResizeObserver(wrapperRef)
+  const keys = Object.keys(data[0].emotions)
 
   useEffect(() => {
     const svg = select(svgRef.current)
     const { width, height } =
       dimensions || wrapperRef.current.getBoundingClientRect()
+
+    const stackGenerator = stack().keys(keys)
+    console.log(stackGenerator(data))
+
+    const xScale = scaleBand()
+      .domain(data.map(d => d.created_at))
+      .range([0, width])
+
+    const xAxis = axisBottom(xScale)
+    svg.select(".x-axis")
+      .attr("transform", `translate(0, ${height})`)
+      .call(xAxis)
+
+
+
+
   }, [colors, data, dimensions, keys])
-
-  const xScale = scaleBand()
-    .domain(data.map(d => d.created_at))
-    .range([0, width])
-
-  const xAxis = axisBottom(xScale)
-  svg.select(".x-axis")
-  .attr("transform", `translate(0, ${height})`)
-  .call(xAxis)
 
   return (
     <React.Fragment>
