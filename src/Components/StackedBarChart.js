3ba483d4e02424f21947d7af9f073a8868a7558a
dyson1602 <cclark000@citymail.cuny.edu>
2021-04-15 15:54:05 -0400

    new: custom hook, useResizeObserver. x axis and x axis scale drawn

--- src/Components/StackedBarChart.js
+++ src/Components/StackedBarChart.js
@@ -1,27 +1,37 @@
-import { select } from 'd3'
-import React, {useEffect, useRef} from 'react'
+import { select, scaleBand, axisBottom, svg } from 'd3'
+import React, { useEffect, useRef } from 'react'
 import useResizeObserver from './useResizeObserver'
 
-function StackedBarChart({data, keys, colors}){
-  const svfRef = useRef()
+function StackedBarChart({ data, keys, colors }) {
+  const svgRef = useRef()
   const wrapperRef = useRef()
   const dimensions = useResizeObserver(wrapperRef)
 
-  useEffect(()=> {
+  useEffect(() => {
     const svg = select(svgRef.current)
-    const {width, height} = 
-    dimensions || wrapperRef.current.getBoundingClientRect()
+    const { width, height } =
+      dimensions || wrapperRef.current.getBoundingClientRect()
   }, [colors, data, dimensions, keys])
 
+  const xScale = scaleBand()
+    .domain(data.map(d => d.created_at))
+    .range([0, width])
 
-  return(
+  const xAxis = axisBottom(xScale)
+  svg.select(".x-axis")
+  .attr("transform", `translate(0, ${height})`)
+  .call(xAxis)
+
+  return (
     <React.Fragment>
-      <div ref={wrapperRef} style={{marginBottom: '2rem'}}>
+      <div ref={wrapperRef} style={{ marginBottom: '2rem' }}>
         <svg ref={svgRef}>
-          <g className="x-axis"/>
-          <g className="y-axis"/>
+          <g className="x-axis" />
+          <g className="y-axis" />
         </svg>
       </div>
     </React.Fragment>
   )
 }
+
+export default StackedBarChart
