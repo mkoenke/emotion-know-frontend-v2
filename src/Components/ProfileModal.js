1bc6c74bed7c484bb7ff5f8128377f0a5ba36110
dyson1602 <cclark000@citymail.cuny.edu>
2021-05-25 22:09:21 -0400

    changed: 'Cancel' to 'Close'

--- src/Components/ProfileModal.js
+++ src/Components/ProfileModal.js
@@ -224,7 +224,7 @@ class ProfileModal extends React.Component {
                   onClick={this.handleCancel}
                   type="cancel"
                 >
-                  Cancel
+                  Close
                 </Button>
               </Modal.Actions>
             </Modal>
