9132c52474d01fe4f7b6e6b462714d41eca438c3
dyson1602 <cclark000@citymail.cuny.edu>
2021-06-08 19:02:57 -0400

    font styled

--- src/Components/GraphDisplayToggleButton.js
+++ src/Components/GraphDisplayToggleButton.js
@@ -1,14 +1,12 @@
-import React, { useState } from 'react';
-
 export const GraphDisplayToggleButton = ({ isToggled, onToggle }) => {
   return (
     <div>
-      <label>Line Graph</label>
+      <label class="content">Line Graph</label>
       <label class="switch">
         <input type="checkbox" checked={isToggled} onChange={onToggle} />
         <span class="slider round" />
       </label>
-      <label>Bar Chart</label>
+      <label class="content">Bar Chart</label>
     </div>
   );
 };
